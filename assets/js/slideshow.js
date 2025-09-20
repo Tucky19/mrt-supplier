c2e7b4a56f6ef09cac823ac49f4bb546ef12046f
Tucky19 <mrt-supplier@outlook.com>
2025-09-20 07:55:52 +0700

    Add files via upload

---
+++ assets/js/slideshow.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+ const slides=document.querySelectorAll(".slideshow .slide");
+ let index=0;
+ setInterval(()=>{
+   slides[index].classList.remove("active");
+   index=(index+1)%slides.length;
+   slides[index].classList.add("active");
+ },3000);
+});
