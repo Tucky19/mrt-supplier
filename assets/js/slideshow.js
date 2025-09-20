16a669d42ce035679bf86fe614ca7b3030f0aa89
Tucky19 <mrt-supplier@outlook.com>
2025-09-20 07:54:12 +0700

    Delete assets directory

--- assets/js/slideshow.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
- const slides=document.querySelectorAll(".slideshow .slide");
- let index=0;
- setInterval(()=>{
-   slides[index].classList.remove("active");
-   index=(index+1)%slides.length;
-   slides[index].classList.add("active");
- },3000);
-});
